2d96373f2ed19e8249b8de29704ff788a1181c3b
Samarth Narayan <kayaniv321@gmail.com>
2024-07-14 00:54:09 +0530

    build gradle setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:interactify/screens/home_screen.dart';
 import 'package:interactify/screens/login_screen.dart';
 import 'package:interactify/utils/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
