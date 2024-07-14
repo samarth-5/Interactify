6d9fbddf4c8dbf474ddbf6e66887db01bbc129e7
Samarth Narayan <kayaniv321@gmail.com>
2024-07-14 21:02:22 +0530

    displaying user meetings

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -5,6 +5,9 @@ class FirestoreMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Stream<QuerySnapshot<Map<String, dynamic>>> get meetingsHistory => _firestore.collection('users').doc(_auth.currentUser!.uid).collection('meetings').snapshots();
+   
+
   void addToMeetingHistory(String meetingName) async {
     try{
       await _firestore.collection('users').doc(_auth.currentUser!.uid).collection('meetings').add({
--- lib/screens/history_meeting_screen.dart
+++ lib/screens/history_meeting_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:interactify/resources/firestore_methods.dart';
 
 class HistoryMeetingScreen extends StatelessWidget {
   const HistoryMeetingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: FirestoreMethods().meetingsHistory,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: No Meetings attended!'),
+          );
+        }
+
+        if (!snapshot.hasData || (snapshot.data as dynamic).docs.isEmpty) {
+          return const Center(
+            child: Text('No meeting history available'),
+          );
+        }
+
+        var docs = (snapshot.data as dynamic).docs;
+
+        return ListView.builder(
+          itemCount: docs.length,
+          itemBuilder: (context, index) {
+            var meetingData = docs[index];
+            return ListTile(
+              title: Text(
+                'Room name: ${meetingData['meetingName']}',
+              ),
+              subtitle: Text(
+                'Joined on: ${meetingData['createdAt']}',
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
